b1d54d956be23788548a3aa4463874dd35b9a847
Ezequiel Soeiro <Ezequielg.soeiro@gmail.com>
2026-03-30 17:47:25 -0300

    tirei essa bendita chave

--- screenmatch/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGpt.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGpt.java
@@ -6,7 +6,7 @@ import com.theokanning.openai.service.OpenAiService;
 public class ConsultaChatGpt {
 
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("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");
+        OpenAiService service = new OpenAiService("");
 
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
